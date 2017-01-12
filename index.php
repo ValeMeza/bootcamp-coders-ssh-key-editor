e78b2a0f28566d6833ede17c592af7543a9c821e
Valente Meza <kingracoon64@gmail.com>
2017-01-12 16:44:26 -0700

    Grammer clean up.

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
     </ul>
     <h2>Relations</h2>
     <ul>
-        <li>One profile per log in. (1 to 1) </li>
+        <li>One log in per profile. (1 to 1) </li>
         <li>One Profile can upload many ssh-keys (1 to n)</li>
         <li>SSH-keys can be added and deleted (n to m)</li>
     </ul>
