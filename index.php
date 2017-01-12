5b84c6d4b70a3fb062b72e72f9d63ac79502866f
Valente Meza <kingracoon64@gmail.com>
2017-01-12 09:00:56 -0700

    Added <ul></ul> as well as the listed items name, age..etc

--- index.php
+++ index.php
@@ -3,11 +3,21 @@
     <head>
         <meta charset="utf-8"
     </head>
-    <title>SSH Key-Editor</title>
+    <title>SSH-Key Editor Example</title>
     <header>
         SSH-Key User
     </header>
 <body>
-<main></main>
+<main> Exaple User
+    <ul>
+        <li> Name: Malente Veza</li>
+        <li>Age: 26</li>
+        <li>Occupation: Unemployed</li>
+        <li>Technology: Has lower end HP laptop running windows 10</li>
+        <li> Needs: Be able to follow the screncast on how to get to and edit SSH-Key editor on bootcamp-coders.cnm.edu</li>
+        <li>Will be able to copy past/delete SSH-Keys into bootcamp-coders.cnm.edu</li>
+        Use Case
+
+</main>
 </body>
 </html>
