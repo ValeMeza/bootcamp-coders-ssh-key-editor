134f67f119db38f9b0a848618f5b7f2c4da39190
Valente Meza <kingracoon64@gmail.com>
2017-01-12 09:55:40 -0700

    Example flow list added as well as conceptual model title.

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     </head>
     <title>SSH-Key Editor Example</title>
     <header>
-        SSH-Key User
+        <h1> SSH-Key User </h1>
     </header>
 <body>
-<main> Exaple User
+<main> <h2>Exaple User</h2>
     <ul>
         <li> Name: Malente Veza</li>
         <li>Age: 26</li>
@@ -16,7 +16,24 @@
         <li>Technology: Has lower end HP laptop running windows 10</li>
         <li> Needs: Be able to follow the screncast on how to get to and edit SSH-Key editor on bootcamp-coders.cnm.edu</li>
         <li>Will be able to copy past/delete SSH-Keys into bootcamp-coders.cnm.edu</li>
-        Use Case
+    </ul>
+        <h2>Use Case</h2>
+        Malente has just spent 30 min to an hour watching and rewatching SSH-key edioter for his bootcamp coders class. After pasting a SSH key he realized he realized he entered the wrong number of bits for his SSH-key. Luckily the universal symbole for delete is located right next to his previous SSH-key. He deletes and adds his new SSH-key.
+        <h2>Example Flow</h2>
+        <ul>
+            <li>Opens boot-coders.cnm.edu/prework.</li>
+            <li>Server fetches information.</li>
+            <li>Hits SSH-key editor and page asked for log in.</li>
+            <li>Page reloads and displays Add SSH key and incorrect SSH-key is displayed.</li>
+            <li>Malente hits delete and pastes new SSH-key. </li>
+            <li>Server deletes and adds new SSH-key.</li>
+            <li>Malente sees the SSH-key added display.</li>
+        </ul>
+    <h2>Conceptual Model</h2>
+    <ul>
+        <li></li>
+    </ul>
+
 
 </main>
 </body>
