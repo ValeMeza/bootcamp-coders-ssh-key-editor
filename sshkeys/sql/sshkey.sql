cece62239e8ff36b7a122a4f7aaa8990b6444ccb
Valente Meza <kingracoon64@gmail.com>
2017-01-16 00:20:56 -0700

    CREATE TABLE profile created for .sql CREATE TABLE sshkey created for .sql CREATE TABLE sshkeys created for .sql

---
+++ sshkeys/sql/sshkey.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS `sshkeys`;
+DROP TABLE IF EXISTS sshkey;
+DROP TABLE IF EXISTS profile;
+CREATE TABLE profile (
+  profileID INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  profileAtHandle VARCHAR(32) NOT NULL,
+  profileEmail Varchar(128) NOT NULL,
+UNIQUE(profileEmail),
+Unique(profileAtHandle),
+PRIMARY KEY(profileID)
+);
+CREATE TABLE sshkey (
+  sshkeyId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  sshkeyProfileId INT UNSIGNED NOT NULL,
+  sshkeyContent VARCHAR(140) NOT NULL,
+  sshkeyDate DATETIME NOT NULL,
+  INDEX(sshkeyProfileId),
+  FOREIGN KEY(sshkeyProfileId) REFERENCES profile(profileId),
+  PRIMARY KEY(sshkeyId)
+);
+CREATE TABLE `sshkeys` (
+  sshkeysProfileId INT UNSIGNED NOT NULL,
+  sshkeysFingerprintId INT UNSIGNED NOT NULL,
+  INDEX(sshkeysProfileId),
+  INDEX(sshkeysFingerprintID),
+  FOREIGN KEY(sshkeysProfileID) REFERENCES profile(profileId),
+  PRIMARY KEY(sshkeysProfileId, sshkeysFingerprintId)
+);
