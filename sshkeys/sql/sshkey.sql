7eeb746229c341a190829c090ce5a680219c70f4
Valente Meza <kingracoon64@gmail.com>
2017-01-17 13:05:43 -0700

    Switched sshkeyContent VARCHAR to CHAR

--- sshkeys/sql/sshkey.sql
+++ sshkeys/sql/sshkey.sql
@@ -6,13 +6,13 @@ CREATE TABLE profile (
   profileAtHandle VARCHAR(32) NOT NULL,
   profileEmail VARCHAR(128) NOT NULL,
 UNIQUE(profileEmail),
-Unique(profileAtHandle),
+UNIQUE(profileAtHandle),
 PRIMARY KEY(profileID)
 );
 CREATE TABLE sshkey (
   sshkeyId INT UNSIGNED AUTO_INCREMENT NOT NULL,
   sshkeyProfileId INT UNSIGNED NOT NULL,
-  sshkeyContent VARCHAR(5000) NOT NULL,
+  sshkeyContent CHAR NOT NULL,
   sshkeyDate DATETIME NOT NULL,
   INDEX(sshkeyProfileId),
   FOREIGN KEY(sshkeyProfileId) REFERENCES profile(profileId),
