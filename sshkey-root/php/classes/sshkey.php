7f0affa43f2598799992314aafe70c9dbfce16e5
Valente Meza <kingracoon64@gmail.com>
2017-01-18 20:29:44 -0700

    added namespace require_once and DOC BLOCK in file sshkey.php

---
+++ sshkey-root/php/classes/sshkey.php
@@ -0,0 +1,12 @@
+<?php
+namespace edu\cnm\vmeza3\bootcamp;
+
+require_once ("sshkey.php");
+
+/**
+ * SSHKEY Class Attempt
+
+    This will be first attempt at a class for SSHKEY Editor Example.
+
+ * @author Valente Meza <vmeza3@cnm.edu>
+ *   @version 3.2.0*/
