70385fd27938b7b07d22794753dc477120cc1b2c
Valente Meza <kingracoon64@gmail.com>
2017-01-19 08:53:56 -0700

    created class Profile

--- sshkey-root/php/classes/sshkey.php
+++ sshkey-root/php/classes/sshkey.php
@@ -6,7 +6,22 @@ require_once ("sshkey.php");
 /**
  * SSHKEY Class Attempt
 
-    This will be first attempt at a class for SSHKEY Editor Example.
+    This will be my first attempt at a class for SSHKEY Editor Example.
 
  * @author Valente Meza <vmeza3@cnm.edu>
  *   @version 3.2.0*/
+
+class Profile implements\j {
+    use ValidateDate;
+    /**
+     * id for this Profile; this is the primary key
+     * @var int $profileId;
+     */
+    private $profileId;
+    /**
+     * id of the ProfileEmail that posted the sshkey
+     @var int $profileEmail*/
+    private $profileEmail;
+    /**  */
+
+}
